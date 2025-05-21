54b689e6300c50f627d31578aff3e3bef80a40b9
Nanapoku97 <nanapoku8732@gmail.com>
2025-05-20 19:43:03 -0500

    if else

---
+++ srs 2.py
@@ -0,0 +1,52 @@
+# names=["ama","kwame","kofi","kwaku"]
+# names[0]=names[0] + " achiaa"
+# names[2]=names[1] + " " + names[3]
+# # print(names)
+#
+# animals=[["tiger","lion"],["ant","butterlfy"]]
+# # print(animals[1][1])
+# x="kwame"
+# # print(x[0])
+# c=names[0]
+# if b:
+#     print(f'{names[2]} stars with k')
+
+# customer=["icecream","macbook","1","ipad","orange","orange","color"]
+# for c in (customer):
+#     break
+#     print(f'the name of the items is {c}')
+
+# student={"Key1":[1,2,3],"Key2":[4,5,6],"Key3":[7,8,9]}
+# import pandas as pd
+# !pip install pandas
+#
+# num1=40
+# num2=30
+# product = num1*num2
+# sum= num1+num2
+# if num1 * num2 <=1000:
+#     print(product)
+# else:
+#     print(sum)
+#
+
+
+
+# str_x = "Emma is good developer. Emma is a writer"
+# # use count method of a str class
+# cnt = str_x.count("Emma")
+# print(cnt)
+
+
+person={'username':'akwasi',
+        'password':'1234',}
+user_name=input('what is your username? \n')
+# print(f'my username is {user_name} ')
+pass_word=input('what is your password? \n')
+# print(f'my password is {pass_word} ')
+if user_name == person['username'] and pass_word == person['password']:
+    print('access granted')
+else:
+    print('access denied')
+
+print('kwame')
