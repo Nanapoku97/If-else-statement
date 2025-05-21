f942040ea741aca6b91b644867cbcfd1271f85c2
Nanapoku97 <nanapoku8732@gmail.com>
2025-05-20 19:39:05 -0500

    my first if else statment

---
+++ nana poku srs.py
@@ -0,0 +1,53 @@
+# fname='jon'
+# lname='doe'
+# print(f'my first name is {fname} and my last name is {lname}')
+# this is known as formatting
+
+# slicing is known as extracting elements from an object
+# for example
+# first_name='adusei'
+# print(first_name[2:5])
+# last_name='frimpong'
+# print(last_name[-8:])
+# print(f'{first_name} {last_name}')
+# other_name='opoku'
+# print(other_name[0:5])
+# interval and examples
+# name='aduseifrimpong'
+# print(name[0::2])
+# using reverse indexing
+# name='aduseifrimpong'
+# print(name[9::-2])
+# Data types and objects
+
+number=[10,20,30,40]
+# length=len(number)
+# if length %2==0:
+#     middle_index=len(number)//2
+#     # print(number[middle_index],number[middle_index-1])
+# else:
+#     print("odd")
+
+# import random
+# colors=["red","yellow","orange","green","purple"]
+# ran = random.randint(0,len(colors)-1)
+# print(ran)
+# if colors[ran]=="green":
+#     print('go')
+# elif colors[ran]=="red":
+#     print('stop')
+# elif colors[ran]=="yellow":
+#     print('get ready')
+# elif colors[ran]=="orange":
+#     print("almost")
+# else:
+#     print("not valid")
+
+
+
+
+
+
+
+
+
---
+++ testing.py
@@ -0,0 +1,7 @@
+a = 30
+b = 25
+c = 20
+if a<b>c:
+    print("real madrid is bigger than barcelona")
+else:
+    print("barcelona is bigger than real madrid")
